39ee79738996a65c5d28cd00770508096e770872
Martin Pettersson <kontakt@martinpettersson.se>
2017-11-27 00:27:11 +0100

    Draw point, clamp, interpolate and process scan line.

--- SoftEngine.ts
+++ SoftEngine.ts
@@ -74,6 +74,86 @@ module SoftEngine {
             return (new BABYLON.Vector3(x, y, point.z));
         }
 
+        public drawPoint(point: BABYLON.Vector2, color: BABYLON.Color4): void {
+            if (point.x >= 0 && point.y >= 0 && 
+                point.x < this.workingWidth && point.y < this.workingHeight) {
+                    this.putPixel(point.x, point.y, color);
+                }
+        }
+
+        public clamp(value: number): number {
+            var min = 0;
+            var max = 1;
+            return Math.max(min, Math.min(value, max));
+        }
+
+        public interpolate(min: number, max: number, gradient: number) {
+            return min + (max - min) * this.clamp(gradient);
+        }
+
+        public processScanLine(y: number, pa: BABYLON.Vector3, pb: BABYLON.Vector3,
+            pc: BABYLON.Vector3, pd: BABYLON.Vector3, color: BABYLON.Color4): void {
+                var gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
+                var gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+                var sx = this.interpolate(pa.x, pb.x, gradient1) >> 0;
+                var ex = this.interpolate(pc.x, pd.x, gradient2) >> 0;
+                for (var x = sx; x < ex; x++) {
+                    this.drawPoint(new BABYLON.Vector2(x, y), color);
+                }
+        }
+
+        public drawTriangle(p1: BABYLON.Vector3, p2: BABYLON.Vector3, 
+                            p3: BABYLON.Vector3,
+                            color: BABYLON.Color4): void {
+            if (p1.y > p2.y) {
+                var temp = p2;
+                p2 = p1;
+                p1 = temp;
+            }
+
+            if (p2.y > p3.y) {
+                var temp = p2;
+                p2 = p3;
+                p3 = temp;
+            }
+
+            if (p1.y > p2.y) {
+                var temp = p2;
+                p2 = p1;
+                p1 = temp;
+            }
+
+            var dP1P2: number; var dP1P3: number;
+
+            if (p2.y - p1.y > 0)
+                dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+            else
+                dP1P2 = 0;
+            
+            if (p3.y - p1.y > 0)
+                dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+            else
+                dP1P3 = 0;
+
+            if (dP1P2 > dP1P3) {
+                for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                    if (y < p2.y) {
+                        this.processScanLine(y, p1, p3, p1, p2, color);
+                    } else {
+                        this.processScanLine(y, p1, p3, p2, p3, color);
+                    }
+                }
+            } else {
+                for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                    if (y < p2.y) {
+                        this.processScanLine(y, p1, p2, p1, p3, color);
+                    } else {
+                        this.processScanLine(y, p2, p3, p1, p3, color);
+                    }
+                }
+            }
+        }
+
         public render(camera: Camera, meshes: Mesh[]): void {
             var viewMatrix = BABYLON.Matrix.LookAtLH(camera.Position, 
                 camera.Target, BABYLON.Vector3.Up());
