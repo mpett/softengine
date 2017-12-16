349d2c48a054173e925092ff47916bfc328934d8
Martin Pettersson <kontakt@martinpettersson.se>
2017-12-16 18:00:29 +0100

    Ranks leap year, write-a-function

--- rank.py
+++ rank.py
@@ -1,3 +1,16 @@
+def leap_year():
+    year = int(input())
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                print("True")
+            else:
+                print("False")
+        else:
+            print("True")
+    else:
+        print("False")
+
 def loops():
     N = int(input())
     for i in range(0, N):
@@ -37,4 +50,4 @@ def hello():
     print("Hello, World!")
 
 if __name__ == '__main__':
-    loops()
+    leap_year()
